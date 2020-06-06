087f278661098203c042d6cb1f39e794fb933559
Anderson <andersonamorim939@gmail.com>
2020-06-06 10:01:58 -0300

    Connect in database

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require("express");
 const app = express();
 const bodyParser = require('body-parser');
+const connection = require('./database/database');
+connection.authenticate()
+  .then((error)=>{
+    console.log('Connect in DB')
+  })
+  .catch(()=>{
+    console.log('Erro connect in DB: ',error)
+  })
 app.set('view engine','ejs')
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({extended:false}));
