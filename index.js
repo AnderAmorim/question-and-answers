8855cb6a0490743727c7a7fb71a6c2e4cedadae9
Anderson <andersonamorim939@gmail.com>
2020-06-05 21:18:55 -0300

    Desenvolvendo layout de perguntar

--- index.js
+++ index.js
@@ -4,21 +4,12 @@ const app = express();
 app.set('view engine','ejs')
 app.use(express.static('public'))
 
+// app.get('/',(req,res)=>{
+//   res.render('content/index')
+// })
+
 app.get('/',(req,res)=>{
-  var nome = "Anderson Amorim";
-  var lang = "JS"
-  var produtos = [
-    {nome:"Doritos",preco:4},
-    {nome:"Coca",preco:2},
-    {nome:"Leite",preco:2},
-  ]
-  res.render('content/index',{
-    nome,
-    lang,
-    empresa:"Voitto",
-    mostrar:true,
-    produtos
-  })
+  res.render('perguntar')
 })
 
 
